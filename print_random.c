fbda54b0ed6b837cc947ed23a3d555c940666c6c
Mark Khvoyevskiy <tuo88860@temple.edu>
2023-09-01 20:35:57 -0400

    Fixed spelling error

--- print_random.c
+++ print_random.c
@@ -10,7 +10,7 @@ int main()
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
 
-	printf("Today'Ss random word: ");
+	printf("Today's random word: ");
 	for(a=0;a<7;a++)
 		putchar( randchar() );
 	putchar('\n');
